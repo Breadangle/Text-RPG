771d2c5af7bf5f5fb534ce12031ae4df29efbbe6
Matthew Cole <mattcole04@gmail.com>
2026-05-10 01:36:48 -0400

    ingame tutorial added
    I forgor.

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -1,10 +1,11 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuFunctions : MonoBehaviour
 {
-
+    public TextMeshProUGUI textMeshProUGUI;
     public void OnStartButton()
     {
         SceneManager.LoadScene("AutoMapTest");
@@ -13,6 +14,18 @@ public class MenuFunctions : MonoBehaviour
     public void OnQuitButton()
     {
         Application.Quit();
+    }
+
+    public void OnHelpButton()
+    {
+        if(textMeshProUGUI.enabled == true)
+        {
+            textMeshProUGUI.enabled = false;
+        }
+        else if (textMeshProUGUI.enabled == false)
+        {
+            textMeshProUGUI.enabled = true;
+        }
     }
 
 }
